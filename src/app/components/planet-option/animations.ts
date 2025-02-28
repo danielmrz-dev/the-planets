0159d2d399ace619b498482ce9d5bc9106486def
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-27 22:38:13 -0300

    d

--- src/app/components/planet-option/animations.ts
+++ src/app/components/planet-option/animations.ts
@@ -1,12 +1,5 @@
 import { animate, query, stagger, style, transition, trigger } from "@angular/animations";
 
 export const stag = trigger('staggingPlanets', [
-    transition('* <=> *', [
-        style({ opacity: 0 }),
-        query('.planets__option', [
-            stagger(100, [
-                animate(500, style({ opacity: 1 }))
-            ])
-        ])
-    ])
+    
 ])
